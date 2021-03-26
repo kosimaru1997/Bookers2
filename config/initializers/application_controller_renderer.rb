8866c72f40303dcc72624489fad294b7fbdd2561
kosimaru1997 <kosimaru1997@gmail.com>
2021-03-26 11:51:04 +0000

    [finish]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3770e02959612f6e3313f13468975bfa73ec33fef186eaca72981a62535a3cd1ee957595123ee964c9a4cfd512f6e6be9e9e2cf8b27e28d745ffccf66ad966cb'
